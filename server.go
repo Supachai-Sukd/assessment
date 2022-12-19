994498adecf98392b144382b5ee7221431fa4596
supachai-sukd <supachai.sukd@gmail.com>
2022-12-19 11:40:03 +0700

    add graceful shutdown #6

--- server.go
+++ server.go
@@ -1,11 +1,43 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/gommon/log"
+	"net/http"
 	"os"
+	"os/signal"
+	"time"
 )
 
 func main() {
-	fmt.Println("Please use server.go for main file")
-	fmt.Println("start at port:", os.Getenv("PORT"))
+	e := echo.New()
+	e.Logger.SetLevel(log.INFO)
+	e.GET("/", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, "OK")
+	})
+
+	// os.Getenv("PORT") Use after refactor.
+	go func() {
+		if err := e.Start(":2565"); err != nil && err != http.ErrServerClosed { // Start server
+			e.Logger.Fatal("shutting down the server")
+		}
+	}()
+	fmt.Printf("start at port: %v\n", 2565) // Port 2565
+
+	// Create channel shutdown
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, os.Interrupt)
+
+	<-shutdown
+
+	// Shutdown in 10 second
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	if err := e.Shutdown(ctx); err != nil {
+		e.Logger.Fatal(err)
+	}
+
 }
